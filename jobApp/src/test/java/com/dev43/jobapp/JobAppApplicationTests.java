908109f7b22e88ace06ffe9b7323babe6aa86d0d
sathiyadevn <sathiyadevnatarajan43@gmail.com>
2024-07-11 12:32:53 +0530

    Basic SpringBoot setup Initiate Model and Controller Layer

---
+++ jobApp/src/test/java/com/dev43/jobapp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev43.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
